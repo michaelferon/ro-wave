c8ee4e645be19da1a012b9e4b1897bc23b22b07d
Jonathan Yun <sukwonyun@gmail.com>
2020-06-10 17:58:07 -0500

    Portraits

--- src/function.R
+++ src/function.R
@@ -4,10 +4,23 @@ library(tidyverse)
 library(lubridate)
 library(ggplot2)
 
-
+load("/Users/jonathanyun/Desktop/ro-wave/data/data.Rdata")
 
 
 # Water permeability Function
-water_perm_coeff <- function(flux, feed_pressure, osmotic_press_rej){
+water_perm_coeff <- function(flux, feed_pressure, rej_cond){
+  #conversion 
+  rej_in_psi <- (rej_cond * 0.5) * 10
   
+  water_coeff <- flux / (feed_pressure - rej_in_psi)
+  return(water_coeff)
 }
+
+View(data)
+
+data <- data %>%
+          mutate(water_perm = water_perm_coeff(water_flux_lmh,
+                                               feed_pressure_psi,
+                                               rej_cond_ms))
+
+
