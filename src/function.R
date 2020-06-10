76ff00ed9c50d97672f551cc064756a51fa56c37
Jonathan Yun <sukwonyun@gmail.com>
2020-06-10 17:08:26 -0500

    added function and pic for the MEMES

---
+++ src/function.R
@@ -0,0 +1,13 @@
+rm(list = ls())
+
+library(tidyverse)
+library(lubridate)
+library(ggplot2)
+
+
+
+
+# Water permeability Function
+water_perm_coeff <- function(flux, feed_pressure, osmotic_press_rej){
+  
+}
