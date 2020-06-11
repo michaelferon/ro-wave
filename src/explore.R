a28cf42ed9023d0ed47bd4d5b0a6a0a372535223
Michael Feron <feron.michael@gmail.com>
2020-06-11 08:38:40 -0600

    Added scatterplot matrices.

--- src/explore.R
+++ src/explore.R
@@ -13,7 +13,7 @@ load('../data/data.Rdata')
 
 ## Number of experiments = 9.
 N <- 8
-OUTPUT <- FALSE
+OUTPUT <- TRUE
 
 
 ## Experiments summarries.
@@ -49,25 +49,43 @@ if (OUTPUT) {
 ## Scatterplot matrices.
 for (i in 1:N) {
   if (OUTPUT) {
-    pdf(file = paste('../plots/scatter_matrix/scatter_matrix', i,
+    pdf(file = paste('../plots/scatter_matrix/corr/corr', i,
                      '.pdf', sep = ''), height = 10.0, width = 10.0)
-  }
-  data %>%
-    filter(experiment == i) %>%
-    select(water_flux_lmh, feed_pressure_psi, feed_volume_l, feed_flow_lm,
-           feed_pump_pow, perm_flow_lm, perm_cond_low_us, rej_cond_ms) %>%
-    rename(
-      flux = water_flux_lmh,
-      feed_psi = feed_pressure_psi,
-      feed_vol = feed_volume_l,
-      feed_flow = feed_flow_lm,
-      feed_pow = feed_pump_pow,
-      perm_flow = perm_flow_lm,
-      perm_cond = perm_cond_low_us,
-      rej_cond = rej_cond_ms,
-    ) %>%
-    pairs(cex = 0.10) #, col = colors[i])
-  if (OUTPUT) {
+    set.seed(1)
+    data %>%
+      filter(experiment == i) %>%
+      slice(sample(1:nrow(.), 10000, replace = FALSE)) %>%
+      select(water_flux_lmh, feed_pressure_psi, feed_pump_pow, feed_flow_lm,
+             perm_flow_lm, rej_flow_lm, ac_current_a) %>%
+      rename(
+        flux = water_flux_lmh,
+        feed_psi = feed_pressure_psi,
+        feed_pow = feed_pump_pow,
+        feed_flow = feed_flow_lm,
+        perm_flow = perm_flow_lm,
+        rej_flow = rej_flow_lm,
+        ac_curr = ac_current_a
+      ) %>%
+      pairs(cex = 0.01)
+    dev.off()
+    
+    pdf(file = paste('../plots/scatter_matrix/other/other', i,
+                     '.pdf', sep = ''), height = 10.0, width = 10.0)
+    set.seed(1)
+    data %>%
+      filter(experiment == i) %>%
+      slice(sample(1:nrow(.), 10000, replace = FALSE)) %>%
+      select(water_perm_coef, feed_pressure_psi, feed_volume_l, feed_temp_c,
+             perm_cond_low_us, rej_cond_ms) %>%
+      rename(
+        perm_coef = water_perm_coef,
+        feed_psi = feed_pressure_psi,
+        feed_vol = feed_volume_l,
+        feed_temp = feed_temp_c,
+        perm_cond = perm_cond_low_us,
+        rej_cond = rej_cond_ms
+      ) %>%
+      pairs(cex = 0.01)
     dev.off()
   }
 }
--- src/init.R
+++ src/init.R
@@ -41,7 +41,7 @@ data <- data %>%
     temp_valve_open = `Temperatre Valve Open (%)`,
     rej_valve_open = `Reject Valve Open (%)`,
     water_flux_lmh = `Water Flux (LMH)`,
-    total_perm_l = `Total Permeate  (L)`,
+    perm_total_l = `Total Permeate  (L)`,
     ac_current_a = `AC Current (A)`,
     perm_tank_level = `Permeate Tank Level`,
     feed_clean_valve_state = `Feed Cleaning Valve State`,
@@ -60,12 +60,29 @@ data$time <- paste(mdy(data$date), data$time) %>%
   parse_date_time('%Y-%m-%d %H:%M:%S %p')
 
 # Remove unnecessary variables.
-del <- c('date', 'runtime_h', 'iter', 'temp_valve_open', 'perm_tank_level',
-         'feed_clean_valve_state', 'perm_dis_valve_state', 'code', 'source',
-         'lj_string')
+del <- c('date', 'runtime_h', 'iter', 'temp_valve_open', 'perm_total_l',
+         'perm_tank_level', 'feed_clean_valve_state', 'perm_dis_valve_state',
+         'code', 'source', 'lj_string')
 data <- data %>%
   select(-all_of(del)) %>%
   mutate_at(c('mode', 'status', 'experiment'), as.factor)
+
+
+# Water permeability coefficient function.
+water_perm_coef <- function(flux, feed_pressure, rej_cond) {
+  rej_in_psi <- rej_cond * 0.5 * 10
+  water_coef <- flux / (feed_pressure - rej_in_psi)
+  return(water_coef)
+}
+# Add water permeability coefficient and rearrange variables.
+data <- data %>%
+  mutate(
+    water_perm_coef = water_perm_coef(water_flux_lmh, feed_pressure_psi, rej_cond_ms)
+  ) %>%
+  select(time, experiment, water_perm_coef, water_flux_lmh, feed_pressure_psi,
+         feed_pump_pow, feed_volume_l, feed_temp_c, feed_flow_lm,
+         perm_cond_low_us, perm_cond_high_us, perm_flow_lm, rej_cond_ms,
+         rej_flow_lm, rej_valve_open, ac_current_a, mode, status)
 
 
 ## Save data.
