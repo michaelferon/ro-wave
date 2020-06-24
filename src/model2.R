80f6877e9b5964b2f47b125883763704103291bb
Michael Feron <feron.michael@gmail.com>
2020-06-24 13:55:27 -0600

    Added model2.R.

---
+++ src/model2.R
@@ -0,0 +1,18 @@
+rm(list = ls())
+
+## Load libraries.
+library(lubridate)
+library(viridis)
+library(ggplot2)
+library(tidyr)
+library(readr)
+library(dplyr)
+
+## Load data.
+load('../data/data.Rdata')
+load('../data/bigdfs.Rdata')
+
+N <- 8
+
+
+
